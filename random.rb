0bc7cb4690357df1d47bbf91cdf61bb71e376a75
melons23 <isdom007@icloud.com>
2024-02-23 18:49:39 +0900

    group task

---
+++ random.rb
@@ -0,0 +1,19 @@
+
+class Random
+  def initialize(group,number)
+    @group = group
+    @number = number
+  end
+  
+  def split
+    [ @group[0..@number-1].sort.to_a , @group[@number..5].sort.to_a ]
+  end
+end 
+
+group = ('A'..'F').to_a
+number = [2,3]
+result = Random.new(group.shuffle, number.sample).split
+
+result.each do |half|
+  p half
+end
